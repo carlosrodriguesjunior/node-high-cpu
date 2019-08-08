ccc56e2dfcb1bab97fcbd1567c860b83739a7c74
Carlos de Souza Rodrigues Júnior <carlosrodriguesjunior@gmail.com>
2019-08-08 16:07:50 -0300

    Update hello-world-server.js

--- hello-world-server.js
+++ hello-world-server.js
@@ -3,7 +3,7 @@ require('http')
   .createServer((req, res) => {
     console.log('url:', req.url)
     console.log('now we will slow down and block and consume CPU...')
-    for (var i=0; i< 1000000; i++) {}
+    for (var i=0; i< 9000000; i++) {}
     res.end('hello world')
   })
   .listen(port, (error)=>{
